548dd4f16e0333383e1caaed6cb86f6b46f1a361
Music-Genre_and-Artist-Prediction <mayurkale@gmail.com>
2025-04-24 14:37:22 +0530

    update

---
+++ edit.php
@@ -0,0 +1,85 @@
+
+<?php
+       
+       include("connection.php");
+       $email = $_GET["myVar"];
+       
+       $myQuery = "SELECT * FROM student WHERE email='".$email."'";
+       $result = $conn->query($myQuery);
+       if( $result->num_rows > 0)
+       {
+           while($row = $result->fetch_assoc() )
+           {
+               $fn = $row["fullname"]; 
+               $em = $row["email"];
+               $cn = $row["contact"];
+               echo '
+                   <tr>
+                       <td>'.$fn.'</td>
+                       <td>'.$em.'</td>
+                       <td>'.$cn.'</td>
+                   </tr>
+               ';
+           }
+       }
+       else
+       {
+           echo "Not found";
+       }
+   ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <!-- Latest compiled and minified CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+   <!-- Latest compiled JavaScript -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+   <div class="container">
+       <div class="row p-5 bg-warning">
+           <div class="col-md-6 p-5">
+               <form action="update.php" method="POST" enctype="multipart/form-data">
+                   Fullname: 
+                   <input type="text" name="fname" placeholder="Enter name" class="form-control" value="<?php echo $fn; ?>">
+                   Email: 
+                   <input type="email" name="email" placeholder="Enter email" class="form-control" value="<?php echo $em; ?>">
+                   Contact: 
+                   <input type="text" name="contact" placeholder="Enter contact" class="form-control" value="<?php echo $cn; ?>">
+                   <br>
+                   <input type="text" placeholder="Enter Password" name="password" class="form-control">
+                   <br>
+
+                   <input type="radio" placeholder="Enter 
+                   Password" name="gender" value="male">Male
+                   <input type="radio" placeholder="Enter Password" name="gender" value="female">Female
+                   <br>
+
+                   <select name="branch" class="form-select">
+                       <option value="21">CSE</option>
+                       <option value="22">IT</option>
+                   </select>
+                   <br>
+                   <input type="file" name="fileToUpload" class="form-control">
+                   <br>
+                   <input type="submit" class="btn btn-success" name="submit">
+               </form>
+           </div>
+           <div class="col-md-6" p-5>
+             <a href="select.php" class="btn btn-primary">View Data</a>
+           </div>
+       </div>
+
+       <div class="row">
+           <form action="selectData.php">
+               <input type="text" name="email" id="" placeholder="enter email to search">
+               <input type="submit">
+           </form>
+       </div>
+   </div>
+</body>
+</html>
--- insert.php
+++ insert.php
@@ -1,39 +1,88 @@
 <?php
     include("connection.php");
 
-    if(  isset( $_GET["submit"]  ) )
-    {   
-        
-        $f_name = trim($_GET["fname"]);
-        $s_email = trim($_GET["email"]);
-        $s_contact = trim($_GET["contact"]);
-        $s_password = trim($_GET["password"]);
-        $gender = trim($_GET["gender"]);
-        $branch = trim($_GET["branch"]);
+    if(  isset( $_POST["submit"]  ) )
+    {
+
+        $target_dir = "uploads/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+        }
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+        }
+
+        // Check file size
+        if ($_FILES["fileToUpload"]["size"] > 500000000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+        $f_name = trim($_POST["fname"]);
+        $s_email = trim($_POST["email"]);
+        $s_contact = trim($_POST["contact"]);
+        $s_password = trim($_POST["password"]);
+        $gender = trim($_POST["gender"]);
+        $branch = trim($_POST["branch"]);
 
         $enc_password = password_hash($s_password , PASSWORD_DEFAULT   );
 
-        $myQuery = $conn->prepare("INSERT INTO student(fullName, email, contact, password, gender,branch) VALUES(?,?,?,?,?,?)");
+        $myQuery = $conn->prepare("INSERT INTO student(fullname, email, contact, password, gender,branch,photo) VALUES(?,?,?,?,?,?,?)");
 
-        $myQuery->bind_param("ssssss",$f_name,$s_email,$s_contact,$s_password,$gender,$branch);
+        $myQuery->bind_param("sssssss",$f_name,$s_email,$s_contact,$enc_password,$gender,$branch,$target_file);
 
         if( $myQuery->execute() == TRUE)
         {
-            echo "
-                <script type='text/javascript'>
-                    alert('Data Inserted');
-                    window.location.href='demoSelect.php';
-                </script>
-            ";
+            echo "OK";
+            // echo "
+            //     <script type='text/javascript'>
+            //         alert('Data Inserted');
+            //         window.location.href='demoSelect.php';
+            //     </script>
+            // ";
         }
         else
         {
-            echo "
-            <script type='text/javascript'>
-                alert('Something went wrong');
-                window.location.href='registration.html';
-            </script>
-            ";
+            echo "Not OK";
+            // echo "
+            // <script type='text/javascript'>
+            //     alert('Something went wrong');
+            //     window.location.href='registration.html';
+            // </script>
+            // ";
         }
     }
     else
